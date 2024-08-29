d81190ad55b1042eb5c2ab9e2da99dbbff191b08
Meet Patel <159328007+mpatel029@users.noreply.github.com>
2024-08-29 15:41:00 -0400

    Update test1.py

--- tests/test1.py
+++ tests/test1.py
@@ -1,6 +1,6 @@
-
-
-from myfile import multiply
-
-def test_multiply_success():
-    assert multiply(2, 3) == 6  # This should pass
+import sys
+import os
+sys.path.append((os.path.join(os.path.dirname(__file__), '..')))
+from myfile import factorial
+def test_case():
+    assert factorial(3) == 6
