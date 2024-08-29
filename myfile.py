0e6a656b47f019812334d780cd4d190736b65281
mpatel029 <159328007+mpatel029@users.noreply.github.com>
2024-08-29 16:43:25 +0000

    Added myfile.py

---
+++ myfile.py
@@ -0,0 +1,10 @@
+def multiply(x, y):
+    return x * y
+
+result = multiply(5, 10)
+print("The result is: " + result)  # Intentional error
+
+def print_message():
+    print("This is a message.")
+
+print_message()
